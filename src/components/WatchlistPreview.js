9b976316409a2b38969700d9f61c9e291b9fe899
bannigeri17 <bannige@emory.edu>
2020-09-28 01:43:57 -0400

    Add modal view and proper fetches from TMDB with Hooks

--- src/components/WatchlistPreview.js
+++ src/components/WatchlistPreview.js
@@ -1,6 +1,118 @@
-import React, {Component} from "react";
-import {StyleSheet, Text, View, ScrollView, FlatList, Image} from "react-native";
+import React, {Component, useState, useEffect} from "react";
+import {
+    StyleSheet,
+    Text,
+    View,
+    ScrollView,
+    FlatList,
+    Image,
+    ActivityIndicator,
+    TouchableOpacity,
+    Modal,
+    Alert,
+    TouchableHighlight
+} from "react-native";
+import * as firebase from "firebase";
+import "firebase/firestore";
 
+// Set the configuration for your app
+const config = {
+    apiKey: "AIzaSyDRW8Lh0Mn2Dv7PZfLkaPFmtXBZEaLpMgw",
+    authDomain: "squadwatch-e2133.firebaseapp.com",
+    databaseURL: "https://squadwatch-e2133.firebaseio.com",
+    storageBucket: "squadwatch-e2133.appspot.com",
+    projectId: "squadwatch-e2133",
+    appId: "squadwatch"
+};
+
+!firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
+
+const firestore = firebase.firestore();
+
+const TMDB_KEY = "fd6611bdf7d4a175b6de314125440173";//process.env['TMDB_KEY'];
+
+function Watchlist(listID) {
+    console.log(listID);
+
+
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+
+    firestore
+        .collection('watchList')
+        .doc(listID)
+        .get()
+        .then((doc) => {
+            console.log(doc.data());
+            setLoading(false);
+            setMovies(doc.data());
+        });
+
+    useEffect(() => {
+        firestore
+            .collection('watchList')
+            .doc(listID)
+            .get()
+            .then((doc) => {
+
+                console.log(doc.data());
+                setLoading(false);
+                setMovies(doc.data());
+            });
+    }, []);
+
+    if (loading) {
+        return <ActivityIndicator/>;
+    } else {
+        return movies;
+    }
+
+}
+
+function fetch_movie_data(movie_id) {
+    const requestStr = "https://api.themoviedb.org/3/movie/" + movie_id + "?api_key=" + TMDB_KEY;
+    const [loading, setLoading] = useState(true);
+    const [movie, setMovie] = useState({});
+
+
+    useEffect(() => {
+        fetch(requestStr)
+            .then((response) => {
+                console.log(response.headers);
+                return response.json();
+            })
+            .then((json) => {
+                //console.log(movie_id);
+                setMovie(json);
+                console.log(json);
+            }).catch((error) => {
+            console.log("Error!: " + error);
+        }).finally(() => setLoading(false)
+        );
+    }, []);
+
+    if (loading) {
+        return <ActivityIndicator/>;
+    } else {
+        return <Text>{movie.original_title + "\n" + movie.overview}</Text>;
+    }
+}
+
+const MovieData = ({
+                       id,
+                   }) => {
+    return fetch_movie_data(id);
+};
+
+console.log(TMDB_KEY);
+
+const test_movie = fetch("https://api.themoviedb.org/3/movie/500?api_key=" + TMDB_KEY);
+
+// test_movie
+//     .then((response) => response.json())
+//     .then((json) => {
+//         console.log(json);
+//     });
 
 const test_watchlist = {
     creatorID: "5UOPtbbQM03QIVUzwNFn",
@@ -18,17 +130,36 @@ const test_watchlist = {
     ]
 };
 
-const Thumbnail = ({title, posterPath}) => (
-    <View style={styles.item}>
-        <Text> {title} </Text>
-        <Image source={{uri: ("https://image.tmdb.org/t/p/w1280" + posterPath)}}
-               style={{width: 70, height: 105}}/>
-    </View>
-);
+const Thumbnail = ({id, title, posterPath}) => {
+    const [modalVisible, setModalVisible] = useState(false);
+    return (<View style={styles.item}>
+            <Modal animationType="slide" transparent={true}
+                   visible={modalVisible} onRequestClose={() => {
+                Alert.alert("Modal view closed");
+            }}>
+                <View padding={22} flex={1} justifyContent={"center"} alignItems={"center"} backgroundColor={"blue"}>
+                    <TouchableHighlight
+                        onPress={() => {
+                            setModalVisible(!modalVisible)
+                        }}>
+                        <MovieData id={id}/>
+                    </TouchableHighlight>
+                </View>
+            </Modal>
+            <TouchableOpacity onPress={() => {
+                setModalVisible(true);
+            }}>
+                <Image source={{uri: ("https://image.tmdb.org/t/p/w1280" + posterPath)}}
+                       style={{width: 70, height: 105}}>
 
+                </Image>
+            </TouchableOpacity>
+        </View>
+    );
+};
 
 const renderItem = ({item}) => (
-    <Thumbnail title = {item.name} posterPath={item.posterPath}/>
+    <Thumbnail id={item.id} title={item.name} posterPath={item.posterPath}/>
 );
 
 export default class WatchlistPreview extends Component {
