ad8bc4d044af839e3a0f6d3a46200c5f64ff2357
bannigeri17 <bannige@emory.edu>
2020-09-30 01:27:21 -0400

    Create ThumbnailList component

--- src/components/ThumbnailList.js
+++ src/components/ThumbnailList.js
@@ -1,74 +1,18 @@
-import React, {Component, useState, useEffect} from "react";
+import React, {Component, useEffect, useState} from "react";
 import {
-    StyleSheet,
-    Text,
-    View,
-    ScrollView,
+    ActivityIndicator,
+    Alert,
     FlatList,
     Image,
-    ActivityIndicator,
-    TouchableOpacity,
     Modal,
-    Alert,
-    TouchableHighlight
+    ScrollView, StyleSheet,
+    Text,
+    TouchableHighlight,
+    TouchableOpacity,
+    View
 } from "react-native";
-import * as firebase from "firebase";
-import "firebase/firestore";
-import ThumbnailList from "./ThumbnailList";
 
-// Set the configuration for your app
-const config = {
-    apiKey: "AIzaSyDRW8Lh0Mn2Dv7PZfLkaPFmtXBZEaLpMgw",
-    authDomain: "squadwatch-e2133.firebaseapp.com",
-    databaseURL: "https://squadwatch-e2133.firebaseio.com",
-    storageBucket: "squadwatch-e2133.appspot.com",
-    projectId: "squadwatch-e2133",
-    appId: "squadwatch"
-};
-
-!firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
-
-const firestore = firebase.firestore();
-
-const TMDB_KEY = "fd6611bdf7d4a175b6de314125440173";//process.env['TMDB_KEY'];
-
-function Watchlist(listID) {
-    console.log(listID);
-
-
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-
-    firestore
-        .collection('watchList')
-        .doc(listID)
-        .get()
-        .then((doc) => {
-            console.log(doc.data());
-            setLoading(false);
-            setMovies(doc.data());
-        });
-
-    useEffect(() => {
-        firestore
-            .collection('watchList')
-            .doc(listID)
-            .get()
-            .then((doc) => {
-
-                console.log(doc.data());
-                setLoading(false);
-                setMovies(doc.data());
-            });
-    }, []);
-
-    if (loading) {
-        return <ActivityIndicator/>;
-    } else {
-        return movies;
-    }
-
-}
+const TMDB_KEY = "fd6611bdf7d4a175b6de314125440173";
 
 function fetch_movie_data(movie_id) {
     const requestStr = "https://api.themoviedb.org/3/movie/" + movie_id + "?api_key=" + TMDB_KEY;
@@ -116,41 +60,6 @@ const MovieData = ({
     return fetch_movie_data(id);
 };
 
-console.log(TMDB_KEY);
-
-const test_movie = fetch("https://api.themoviedb.org/3/movie/500?api_key=" + TMDB_KEY);
-
-// test_movie
-//     .then((response) => response.json())
-//     .then((json) => {
-//         console.log(json);
-//     });
-
-const test_watchlist = {
-    creatorID: "5UOPtbbQM03QIVUzwNFn",
-    movies: [
-        {
-            id: "500",
-            name: "Reservoir Dogs",
-            posterPath: "/AjTtJNumZyUDz33VtMlF1K8JPsE.jpg"
-        },
-        {
-            id: "600",
-            name: "Full Metal Jacket",
-            posterPath: "/kMKyx1k8hWWscYFnPbnxxN4Eqo4.jpg"
-        },
-        {
-            id: "601",
-            name: "E.T. the Extra-Terrestrial",
-            posterPath: "/pEKStszBzTZCvR0H4tosjqxmE7X.jpg"
-        },
-        {
-            id: "346",
-            name: "Seven Samurai",
-            posterPath: "/1wCRVLGI7SoTOoDRzWlbt2dMDuy.jpg"
-        },
-    ]
-};
 
 const Thumbnail = ({id, title, posterPath}) => {
     const [modalVisible, setModalVisible] = useState(false);
@@ -168,7 +77,7 @@ const Thumbnail = ({id, title, posterPath}) => {
                         }}>
                         <ScrollView>
 
-                        <MovieData id={id}/>
+                            <MovieData id={id}/>
                         </ScrollView>
                     </TouchableHighlight>
                 </View>
@@ -189,19 +98,24 @@ const renderItem = ({item}) => (
     <Thumbnail id={item.id} title={item.name} posterPath={item.posterPath}/>
 );
 
-export default class WatchlistPreview extends Component {
+export default class ThumbnailList extends Component {
+    /**
+     * props:
+     */
+    listTitle;
+    movieList;
+
     render() {
         return (
-            /*<View style={styles.container}>
-                <Text style={styles.text}>MY WATCHLIST</Text>
+            <View style={styles.container}>
+                <Text style={styles.text}>{this.props.listTitle}</Text>
                 <FlatList horizontal={true} showsHorizontalScrollIndicator={false}
-                          data={test_watchlist.movies}
+                          data={this.props.movieList}
                           renderItem={renderItem}
                           keyExtractor={item => item.id}
                 />
-            </View>*/
-            <ThumbnailList listTitle={"MY WATCHLIST"} movieList={test_watchlist.movies}/>
-            );
+            </View>
+        );
     }
 }
 
