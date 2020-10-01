19fa6d19c75f2db86b6e99d84b70039647f47d3e
bannigeri17 <bannige@emory.edu>
2020-09-30 20:10:17 -0400

    configure .env and environment variables for TMDB key and firebase config

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from "expo-status-bar";
+import {StatusBar} from "expo-status-bar";
 import React from "react";
 import ReactDOM from "react-dom";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
-import { NativeRouter, Switch, Route, Router, Link } from "react-router-native";
+import {StyleSheet, Text, View, ScrollView} from "react-native";
+import {NativeRouter, Switch, Route, Router, Link} from "react-router-native";
 import Home from "./src/views/Home.js";
 import MovieList from "./src/views/MovieList.js";
 import Navigation from "./src/components/Navigation.js";
+import {
+    FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN,
+    FIREBASE_DB_URL,
+    FIREBASE_STORAGE_BUCKET,
+    FIREBASE_PROJECT_ID,
+    FIREBASE_APP_ID
+} from '@env';
+import * as firebase from "firebase";
+import "firebase/firestore";
+import "firebase/auth";
+
+//set up firebase configuration from environment variables
+
+const firebase_config = {
+    apiKey: FIREBASE_API_KEY,
+    authDomain: FIREBASE_AUTH_DOMAIN,
+    databaseURL: FIREBASE_DB_URL,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    projectId: FIREBASE_PROJECT_ID,
+    appId: FIREBASE_APP_ID,
+};
+
+!firebase.apps.length ? firebase.initializeApp(firebase_config) : firebase.app();
+
+const firestore = firebase.firestore();
+const firebaseAuth = firebase.auth();
 
 export default function App() {
     return (
