8e034cbea153b7293c0c79013a4cdbeac31a6cad
Kcode <Ktuyish@emory.edu>
2020-10-08 03:43:49 +0200

    restoring some styles + finishing touches

--- src/styles/Colors.js
+++ src/styles/Colors.js
@@ -1,14 +1,14 @@
 'use strict';
 
 var Colors ={
-    swOrange:'#F56A0D',
+    swOrange:'#D85600',
     swNavy:'#181b3d',
     swGrey:'#23232B',
     swPink:'#FF4DB4',
     swBlack:'#23232B',
-    swPurple:'#7E50B5',
-    swGreen:'#41AF7F',
-    swBlue:'#2982DB',
+    swPurple:'#5B00C9',
+    swGreen:'#00B465',
+    swBlue:'#006ECC',
 };
 
 module.exports = Colors;
