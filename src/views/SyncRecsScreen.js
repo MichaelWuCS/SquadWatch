baf36dac3ef869d32840fc7d3d89f62daf02d094
bannigeri17 <bannige@emory.edu>
2020-10-25 19:21:22 -0400

    style room screen

---
+++ src/views/SyncRecsScreen.js
@@ -0,0 +1,52 @@
+import React, { Component, useEffect, useState } from "react";
+import {
+    Text,
+    View,
+    StyleSheet,
+    FlatList,
+    ActivityIndicator,
+    TouchableOpacity
+} from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import * as firebase from "firebase";
+import "firebase/firestore";
+import "firebase/auth";
+import {TMDB_KEY} from "@env";
+import { swBlack, swBlue, swGreen, swGrey, swPink, swPurple, swOrange } from "../styles/Colors";
+
+
+export default class SyncRecsScreen extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            loading:false,
+            data:[]
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+    makeRemoteRequest(){
+
+    }
+
+    renderTopPick = () =>{
+
+    }
+
+    renderList = () => {
+
+    }
+
+    render() {
+        return (
+            <View>
+                {this.renderTopPick()}
+                {this.renderList()}
+            </View>
+        );
+    }
+}
